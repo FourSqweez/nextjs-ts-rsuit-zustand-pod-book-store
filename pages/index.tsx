8bfdd807d8faffe3d930b7cbe6cc2acd7a9a68f8
FourSqweez <jeerasak.kr@chomchob.com>
2022-11-09 17:21:09 +0700

    try to fetch data using axios

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,24 @@
+import axios from 'axios'
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-     
-    </div>
-  )
+interface HomeTypeProps {
+	booksData: []
+}
+
+export default function Home(props: HomeTypeProps) {
+	console.log('data :', props.booksData)
+	return <div className={styles.container}></div>
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+	//const params = context.params! // ! is a non-null assertion
+	const booksData = await axios
+		.get(`https://json-server-book-store-pod.herokuapp.com/books`)
+		.then((res) => res.data)
+	return {
+		props: { booksData },
+	}
 }
