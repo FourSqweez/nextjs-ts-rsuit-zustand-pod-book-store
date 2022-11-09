dee23cebda03ae4d9d9d5f7122c5fd1fdf3c37c8
FourSqweez <jeerasak.kr@chomchob.com>
2022-11-09 17:55:13 +0700

    created interface and display book title from getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,26 @@ import axios from 'axios'
 import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { type } from 'os'
 import styles from '../styles/Home.module.css'
 
 interface HomeTypeProps {
 	booksData: []
 }
 
+interface BookType {
+	title: string
+}
+
 export default function Home(props: HomeTypeProps) {
 	console.log('data :', props.booksData)
-	return <div className={styles.container}></div>
+	return (
+		<div className={styles.container}>
+			{props.booksData.map((book: { title: string }) => (
+				<div>{book.title}</div>
+			))}
+		</div>
+	)
 }
 
 export const getStaticProps: GetStaticProps = async (context) => {
